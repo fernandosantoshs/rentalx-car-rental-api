c9e12cf2699e4b19fe0221b57ddb1bd587dae034
Fernando Santos <fernandosantoshs@gmail.com>
2023-11-01 10:15:18 -0300

    Refatored createSpecification; added import category from csv files

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,28 +1,11 @@
 import { Router } from 'express';
 
-import { SpecificationRepository } from '../modules/cars/repositories/implementations/SpecificationRepository';
-import { CreateSpecificationUseCase } from '../modules/cars/useCases/createSpecification/CreateSpecificationUseCase';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 
 specificationsRoutes.post('/', (request, response) => {
-  const specificationService = new CreateSpecificationUseCase(
-    specificationRepository
-  );
-  const { name, description } = request.body;
-
-  specificationService.execute({ name, description });
-
-  return response.status(201).send();
+  createSpecificationController.handle(request, response);
 });
 
-specificationsRoutes.get('/', (request, response) => {
-  const specificationService = new CreateSpecificationUseCase(
-    specificationRepository
-  );
-  const listSpecifications = specificationService.list();
-
-  return response.status(200).send(listSpecifications);
-});
 export { specificationsRoutes };
