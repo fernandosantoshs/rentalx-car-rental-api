ad63d1f986c483f438882c87e74d51a3ed0835f9
Fernando Santos <fernandosantoshs@gmail.com>
2023-11-02 18:13:42 -0300

    Remove csv file after import done; started swagger documentation

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -11,4 +11,4 @@ interface ICategoriesRepository {
   create({ name, description }: ICreateCategoryDTO): void;
 }
 
-export { ICategoriesRepository, ICreateCategoryDTO as ICreateCategory };
+export { ICategoriesRepository, ICreateCategoryDTO };
